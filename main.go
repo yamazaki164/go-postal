a5b6e64ea0c3ce5ee59bed242ad981dc4c4ca900
yamazaki164 <yamazaki164@gmail.com>
2017-09-19 20:13:08 +0900

    parse

--- main.go
+++ main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"encoding/csv"
-	"fmt"
+	"encoding/json"
 	"io"
+	"io/ioutil"
 	"os"
-
 	"strconv"
+	"strings"
 
 	"golang.org/x/text/encoding/japanese"
 )
 
 type Postal struct {
 	JisCode         string `json:"jis_code"`
-	OldPostalCode   string `json:"old_postal_code"`
+	OldPostalCode   string `json:"-"`
 	PostalCode      string `json:"postal_code"`
-	PostalCodeShort string `json:"postal_code_short"`
+	PostalCodeShort string `json:"-"`
 	KanaPrefecture  string `json:"kana_prefecture"`
 	KanaAddress1    string `json:"kana_address1"`
 	KanaAddress2    string `json:"kana_address2"`
@@ -31,36 +32,53 @@ type Postal struct {
 }
 
 func NewPostal(r []string) *Postal {
-	flag1, _ := strconv.ParseBool(r[9])
-	flag2, _ := strconv.ParseBool(r[10])
-	flag3, _ := strconv.ParseBool(r[11])
-	flag4, _ := strconv.ParseBool(r[12])
-	status, _ := strconv.ParseInt(r[13], 10, 32)
-	reason, _ := strconv.ParseInt(r[14], 10, 32)
+	flag1, _ := strconv.ParseBool(strings.TrimSpace(r[9]))
+	flag2, _ := strconv.ParseBool(strings.TrimSpace(r[10]))
+	flag3, _ := strconv.ParseBool(strings.TrimSpace(r[11]))
+	flag4, _ := strconv.ParseBool(strings.TrimSpace(r[12]))
+	status, _ := strconv.ParseInt(strings.TrimSpace(r[13]), 10, 32)
+	reason, _ := strconv.ParseInt(strings.TrimSpace(r[14]), 10, 32)
 
 	p := &Postal{
-		JisCode:        r[0],
-		OldPostalCode:  r[1],
-		PostalCode:     r[2],
-		KanaPrefecture: r[3],
-		KanaAddress1:   r[4],
-		KanaAddress2:   r[5],
-		Prefecture:     r[6],
-		Address1:       r[7],
-		Address2:       r[8],
-		Flag1:          flag1,
-		Flag2:          flag2,
-		Flag3:          flag3,
-		Flag4:          flag4,
-		Status:         status,
-		Reason:         reason,
+		JisCode:         strings.TrimSpace(r[0]),
+		OldPostalCode:   strings.TrimSpace(r[1]),
+		PostalCode:      strings.TrimSpace(r[2]),
+		PostalCodeShort: strings.TrimSpace(r[2])[0:3],
+		KanaPrefecture:  strings.TrimSpace(r[3]),
+		KanaAddress1:    strings.TrimSpace(r[4]),
+		KanaAddress2:    strings.TrimSpace(r[5]),
+		Prefecture:      strings.TrimSpace(r[6]),
+		Address1:        strings.TrimSpace(r[7]),
+		Address2:        strings.TrimSpace(r[8]),
+		Flag1:           flag1,
+		Flag2:           flag2,
+		Flag3:           flag3,
+		Flag4:           flag4,
+		Status:          status,
+		Reason:          reason,
 	}
 
 	return p
 }
 
+type PList []*Postal
+
+type areaPostal map[string]PList
+
+func writeJson(k1 string, p areaPostal) {
+	var rootDir = "c:/works/tmp/" + k1
+
+	_, err := os.Stat(rootDir)
+	if err != nil && os.IsNotExist(err) {
+		os.MkdirAll(rootDir, 0644)
+	}
+
+	b, _ := json.Marshal(p)
+	ioutil.WriteFile(rootDir+"/"+k1+".json", b, 0644)
+}
+
 func main() {
-	fin, err := os.OpenFile("g:/tmp/KEN_ALL.CSV", os.O_RDONLY, 0755)
+	fin, err := os.OpenFile("C:/works/gocode/src/github.com/yamazaki164/go-postal/KEN_ALL.CSV", os.O_RDONLY, 0755)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -68,20 +86,35 @@ func main() {
 
 	jdec := japanese.ShiftJIS.NewDecoder()
 
-	cin := csv.NewReader(jdec.Reader(fin))
-	cin.LazyQuotes = true
-	postalList := []*Postal{}
+	reader := csv.NewReader(jdec.Reader(fin))
+	reader.LazyQuotes = true
+
+	var plist map[string]areaPostal = make(map[string]areaPostal)
+
 	for {
-		record, err := cin.Read()
+		record, err := reader.Read()
 		if err == io.EOF {
 			break
 		} else if err != nil {
 			panic(err)
 		} else {
 			p := NewPostal(record)
-			postalList = append(postalList, p)
+
+			if _, ok := plist[p.PostalCodeShort]; ok {
+				if _, ok2 := plist[p.PostalCodeShort][p.PostalCode]; ok2 {
+					plist[p.PostalCodeShort][p.PostalCode] = append(plist[p.PostalCodeShort][p.PostalCode], p)
+				} else {
+					plist[p.PostalCodeShort][p.PostalCode] = PList{p}
+				}
+			} else {
+				plist[p.PostalCodeShort] = areaPostal{
+					p.PostalCode: PList{p},
+				}
+			}
 		}
 	}
 
-	fmt.Println(len(postalList))
+	for k, v := range plist {
+		writeJson(k, v)
+	}
 }
